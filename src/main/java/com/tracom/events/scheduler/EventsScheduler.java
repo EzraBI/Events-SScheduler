87ae5c2472cae737721ae6eccfff5b32758e9937
Ezra <82878692+EzraBI@users.noreply.github.com>
2021-12-01 15:34:46 +0300

    Spring CRUD Template

---
+++ src/main/java/com/tracom/events/scheduler/EventsScheduler.java
@@ -0,0 +1,13 @@
+package com.tracom.events.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsScheduler {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsScheduler.class, args);
+	}
+
+}
